3cdcca290f903133df120ea737e42516b7a053f8
stevesan <stevenan@gmail.com>
2013-02-03 21:56:06 -0800

    level orders and next-level logic

--- unity/Assets/scripts/Game.js
+++ unity/Assets/scripts/Game.js
@@ -10,5 +10,18 @@ function Update () {
     {
         Application.LoadLevel(Application.loadedLevel);
     }
+    else if( Input.GetButtonDown("Skip") )
+    {
+        if( Application.loadedLevel == Application.levelCount-1 )
+        {
+            // game over!
+            Debug.Log("BEAT GAME!");
+        }
+        else
+        {
+            Debug.Log("loading "+(Application.loadedLevel+1));
+            Application.LoadLevel(Application.loadedLevel+1);
+        }
+    }
 
 }
